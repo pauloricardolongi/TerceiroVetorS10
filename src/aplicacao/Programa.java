0eca45e8af5f578f8fd400a308556fc77b680608
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-09 02:41:01 -0300

    Versao Vetor com a Classe

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -6,43 +6,27 @@ import java.util.Scanner;
 import entities.Produto;
 
 public class Programa {
-/*Fazer um programa para ler um número inteiro N e os dados (nome e
-preço) de N Produtos. Armazene os N produtos em um vetor. Em
-seguida, mostrar o preço médio dos produtos.
 
- *Input: Output:                               AVERAGE PRICE = 700.00
-3                                      
-400.00
-Stove
-800.00
-TV
-900.00
-Fryer
-400.00
-Stove
-800.00
-*/
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
+		Locale.setDefault ( Locale.US );
+		Scanner sc =  new  Scanner ( System.in);
 		
-		int n = sc.nextInt();
-		Produto[] vetor = new Produto[n];
-		for (int i=0; i<vetor.length; i++) {
+		int n = sc.nextInt ();
+		Produto[] vetor =  new  Produto [n];
+		for ( int i = 0 ; i < vetor.length; i ++ ) {
 			sc.nextLine();
-			String nome = sc.nextLine();
+			String nome = sc.nextLine ();
 			double preco = sc.nextDouble();
-			vetor[i] = new Produto(nome, preco);
+			vetor [i] =  new  Produto (nome, preco);
 		}
-		double soma= 0.0;
-		for (int i=0; i<vetor.length; i++) {
+		double soma = 0.0;
+		for( int i = 0 ; i < vetor.length; i ++ ) {
 			soma = soma + vetor[i].getPreco();
 		}
 		double media = soma / vetor.length;
-		System.out.printf("A  média de preço é: %.2f%n",  media);	
+		System.out.printf("A Média é= %.2f%n ", media);	
 		
 		sc.close();
-
 	}
 
 }
