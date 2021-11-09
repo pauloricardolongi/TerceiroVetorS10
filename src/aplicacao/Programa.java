5d47df203a62f464c11d3e130b75d0649a44f639
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-09 02:22:46 -0300

    Vetor Segunda Versão

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -3,31 +3,43 @@ package aplicacao;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Produto;
+
 public class Programa {
-/*Fazer um programa para ler um número inteiro N. E a altura de N
-pessoas. Armazene as N alturas em um vetor. Em seguida, mostrar a
-altura média dessas pessoas.
-Input:                                      Output:
-3
-1.72                                      AVERAGE HEIGHT = 1.69
-1.56
-1.80
- */
+/*Fazer um programa para ler um número inteiro N e os dados (nome e
+preço) de N Produtos. Armazene os N produtos em um vetor. Em
+seguida, mostrar o preço médio dos produtos.
+
+ *Input: Output:                               AVERAGE PRICE = 700.00
+3                                      
+400.00
+Stove
+800.00
+TV
+900.00
+Fryer
+400.00
+Stove
+800.00
+*/
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		double[] vetor = new double[n];
-		for (int i=0; i<n; i++) {
-			vetor[i]= sc.nextDouble();
+		Produto[] vetor = new Produto[n];
+		for (int i=0; i<vetor.length; i++) {
+			sc.nextLine();
+			String nome = sc.nextLine();
+			double preco = sc.nextDouble();
+			vetor[i] = new Produto(nome, preco);
 		}
-		double soma= 0;
-		for (int i=0; i<n; i++) {
-			soma = soma + vetor[i];
+		double soma= 0.0;
+		for (int i=0; i<vetor.length; i++) {
+			soma = soma + vetor[i].getPreco();
 		}
-		double media = soma / n;
-		System.out.printf("A altura média é: %.2f%n",  media);	
+		double media = soma / vetor.length;
+		System.out.printf("A  média de preço é: %.2f%n",  media);	
 		
 		sc.close();
 
